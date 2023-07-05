7c87f2d5840028ab1a949c6820d228154c4b965e
wyknmjj <wyknmjj@protonmail.com>
2023-07-05 08:57:22 +0800

    init this project

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <p>
+      <Button variant="secondary" size="icon" >Click Me</Button>
+      this is the first page
+    </p>
+  )
+}
